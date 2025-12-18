f29be75eb307bd399d049745824de0a258bac6b1
nguyenhg2 <clonefull81@gmail.com>
2025-12-18 07:49:14 +0700

    okk

--- AppConfig.cs
+++ AppConfig.cs
@@ -6,6 +6,6 @@ namespace QLGD_WinForm
 {
     public static class AppConfig
     {
-        public static string ConnectionString = "Data Source=AcerNitro5;Initial Catalog=QLGD_new;User ID=sa;Password=Nguyenhg2@;Encrypt=True;Trust Server Certificate=True";
+        public static string ConnectionString = "";
     }
 }
